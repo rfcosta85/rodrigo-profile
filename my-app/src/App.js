5afce42bad5bf0ebce20a6d04955152317a3d5c8
Rodrigo Costa <rfcosta85@gmail.com>
2021-05-15 19:04:04 -0300

    Atualizando Header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
                 <h1 className="text-uppercase text-white h1-title">Eu sou Rodrigo Costa</h1>
                 <span className="d-block text-white sub-title">Front-End Developer</span>
                 <span className="d-block text-white skills mt-2">HTML5, CSS3, JavaScript, Bootstrap e React</span>
-                <button type="button" className="btn mt-5 btn-design page-scroll justify-content-center text-white"><a href="#"></a>saiba mais</button>
+                <button type="button" className="btn mt-5 btn-design page-scroll justify-content-center text-white"><a href="https://www.google.com.br/"></a>saiba mais</button>
             </div>
         </div>
     </aside> 
