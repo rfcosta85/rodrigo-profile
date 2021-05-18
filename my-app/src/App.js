f4b5e0ce79f75a3d730f0005ae95b2447dedf727
Rodrigo Costa <rfcosta85@gmail.com>
2021-05-17 21:03:40 -0300

    Adicionado o nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,9 +12,46 @@ function App() {
                 <span className="d-block text-white sub-title">Front-End Developer</span>
                 <span className="d-block text-white skills mt-2">HTML5, CSS3, JavaScript, Bootstrap e React</span>
                 <button type="button" className="btn mt-5 btn-design page-scroll justify-content-center text-white"><a href="https://www.google.com.br/"></a>saiba mais</button>
-            </div>
+            </div>            
         </div>
-    </aside> 
+
+        <nav id="navigation" className="navbar navbar-dark navbar-expand-lg bg-dark">
+          <div className="container">
+            <div className="navbar-header d-lg-none">
+              <a className="navbar-brand page-scroll">RODRIGO</a>
+
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-rodrigo" aria-controls="navbar-rodrigo" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            </div>
+            <div className="navbar-collapse collapse justify-content-center" id="navbar-rodrigo">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Profile</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Portfolio</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Resume</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Skills</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>     
+      </aside> 
     </div>
   );
 }
