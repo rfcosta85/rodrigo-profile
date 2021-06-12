5dd6c9b8cace03d1fe460046c9950c501cd13d0c
Rodrigo Costa <rfcosta85@gmail.com>
2021-06-12 14:14:41 -0300

    Atualizando layout principal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -14,44 +13,36 @@ function App() {
                 <button type="button" className="btn mt-5 btn-design page-scroll justify-content-center text-white"><a href="https://www.google.com.br/"></a>saiba mais</button>
             </div>            
         </div>
-
-        <nav id="navigation" className="navbar navbar-dark navbar-expand-lg bg-dark">
-          <div className="container">
-            <div className="navbar-header d-lg-none">
-              <a className="navbar-brand page-scroll">RODRIGO</a>
-
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-rodrigo" aria-controls="navbar-rodrigo" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
+        <nav id="navigation" className="navbar navbar-dark navbar-expand bg-dark">
+            <div className="container justify-content-center">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a href="#home" className="nav-link page-scroll">Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="#profile" className="nav-link page-scroll">Profile</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="Contact" className="nav-link page-scroll">Contact</a>
+                    </li>
+                </ul>
             </div>
-            <div className="navbar-collapse collapse justify-content-center" id="navbar-rodrigo">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Profile</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Portfolio</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Services</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Resume</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Skills</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll">Contact</a>
-                </li>
-              </ul>
+        </nav>  
+        <section id="profile" className="bg-gray-dark-2 overflow-hidden py-0 mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-7 col-profile" data-mh="mh-col-profile">
+              <div className="pr-lg-4">
+                <h3 className="font-alt font-w-600 letter-spacing-2 title-extra-large text-uppercase text-white">Olá meu nome é Rodrigo</h3>
+                <p className="mt-4 text-large text-white">Rodrigo Costa, 35 anos, nascido e criado no Rio de Janeiro. Entusiasta do mundo front-end utilizando essa plataforma para por em prática os meus conhecimentos.</p>
+                <span className="bg-base-color mt-4 sep-line-medium-thick"></span>
+                <p className="mt-4 text-secondary text-large">Atualmente sou analista de infra estrutura na Land Rover Jaguar, atuando como estagiário. Além do cargo na Land Rover eu ainda atuo como redator em meu blog pessoal no Medium, onde trato assuntos aprendido durante o período letivo na universidade, e por fim também atuo como front-end developer como freelancer.</p>
+              </div>
             </div>
           </div>
-        </nav>     
-      </aside> 
+        </div>
+      </section>      
+      </aside>      
     </div>
   );
 }
